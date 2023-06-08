2fa8e8d1cdde837accbf3cdfd1bee7ee79a7d3d0
iedaiedaieda <133114301+iedaiedaieda@users.noreply.github.com>
2023-06-08 21:42:43 +0800

    Update index.py

--- index.py
+++ index.py
@@ -11,24 +11,26 @@ from linebot.models import (
 )
 
 import requests
-
-def checkword(w):
-    url = 'https://www.moedict.tw/uni/' + w
-    r = requests.get(url)
-    datas = r.json()
-    msg = '國字：' + datas['title'] + '\n'
-    msg += '部首：' + datas['radical'] + '\n'
-    msg += '筆劃：' + str(datas['stroke_count']) + '\n\n'
-    for i in range(len(datas['heteronyms'])):
-        msg += '注音：' + datas['heteronyms'][i]['bopomofo'] + '\n'
-        msg += '拼音：' + datas['heteronyms'][i]['pinyin'] + '\n'    
-        for j in range(len(datas['heteronyms'][i]['definitions'])):
-            if 'type' in datas['heteronyms'][i]['definitions'][j]:
-                msg += '[{}] {}\n'.format(
-                    datas['heteronyms'][i]['definitions'][j]['type'],
-                    datas['heteronyms'][i]['definitions'][j]['def'])
-        msg += '\n'
-    return msg
+from pypinyin import pinyin, lazy_pinyin, Style
+K = input('請輸入要查詢的字詞:')
+pinyin(K, style=Style.BOPOMOFO)
+#def checkword(w):
+#    url = 'https://www.moedict.tw/uni/' + w
+#    r = requests.get(url)
+#    datas = r.json()
+#    msg = '國字：' + datas['title'] + '\n'
+#    msg += '部首：' + datas['radical'] + '\n'
+#    msg += '筆劃：' + str(datas['stroke_count']) + '\n\n'
+#    for i in range(len(datas['heteronyms'])):
+#        msg += '注音：' + datas['heteronyms'][i]['bopomofo'] + '\n'
+#        msg += '拼音：' + datas['heteronyms'][i]['pinyin'] + '\n'    
+#        for j in range(len(datas['heteronyms'][i]['definitions'])):
+#            if 'type' in datas['heteronyms'][i]['definitions'][j]:
+#                msg += '[{}] {}\n'.format(
+#                    datas['heteronyms'][i]['definitions'][j]['type'],
+#                    datas['heteronyms'][i]['definitions'][j]['def'])
+#        msg += '\n'
+#    return msg
 
 
 app = Flask(__name__)
